ad00521d55d8c45858db8fde6eef04fa8739a0e1
Dimitar Petrov <dimitar.petrov.311@gmail.com>
2025-05-24 17:35:16 +0300

    trace-console-example

--- traces-consoleapp/Program.cs
+++ traces-consoleapp/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
+public class Program
+{
+    private static readonly ActivitySource MyActivitySource = new("MyCompany.MyProduct.MyLibrary");
+
+    public static void Main()
+    {
+        var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .AddSource("MyCompany.MyProduct.MyLibrary")
+            .AddConsoleExporter()
+            .Build();
+
+        using (var activity = MyActivitySource.StartActivity("SayHello", ActivityKind.Client))
+        {
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+            activity?.SetTag("baz", new int[] { 1, 2, 3 });
+            activity?.SetStatus(ActivityStatusCode.Ok);
+        }
+
+        // Dispose tracer provider before the application ends.
+        // This will flush the remaining spans and shutdown the tracing pipeline.
+        tracerProvider.Dispose();
+    }
+}
